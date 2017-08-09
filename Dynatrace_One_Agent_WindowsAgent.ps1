2289eb30f2ebc6eca81f45ba71d135d5ad4ff157
[Akramulla K M] <[akramullak@sysgain.com]>
2017-08-09 17:53:38 +0530

    upload updated powershell script

--- Dynatrace_One_Agent_WindowsAgent.ps1
+++ Dynatrace_One_Agent_WindowsAgent.ps1
@@ -1,4 +1,4 @@
 Invoke-WebRequest -Uri https://github.com/akramullak-sysgain/Dynatrace-OneAgent-Windows/raw/master/Dynatrace-OneAgent-Windows-1.123.225.exe  -OutFile c:/users/Dynatrace-OneAgent-Windows-1.123.225.exe
 c:/users/Dynatrace-OneAgent-Windows-1.123.225.exe /qn
-Start-Sleep -s 90
+Start-Sleep -s 60
 Restart-Service -SERVICENAME "Dynatrace OneAgent"
